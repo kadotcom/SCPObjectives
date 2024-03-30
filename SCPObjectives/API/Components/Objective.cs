6548b1c187bf84d8be84d39cb591e84bc1beb686
KadotCom (Kaiden) <76606192+kadotcom@users.noreply.github.com>
2024-03-30 14:30:03 -0400

    Removed more unused imports and added Descriptions to the Objectives

--- SCPObjectives/API/Components/Objective.cs
+++ SCPObjectives/API/Components/Objective.cs
@@ -1,33 +1,37 @@
 ﻿using PlayerRoles;
 using SCPObjectives.API.Enums;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
 
 namespace SCPObjectives.API.Components
 {
     public class Objective
     {
+        [Description("The objective that you need to complete (read the GitHub README for each ObjectiveType)")]
         public ObjectiveEnum ObjectiveType { get; set; } = ObjectiveEnum.None;
-        
+
+        [Description("Determines if a certain role can get/complete the objective.")]
         public bool IsRoleSpecific { get; set; } = false;
 
+        [Description("List of RoleTypeId's that can get/complete the objective if is_role_specific' is enabled.")]
         public List<RoleTypeId> RolesThatCanGetObjective { get; set; } = new List<RoleTypeId>();
 
+        [Description("The reward you'll get once the objective is completed (read the GitHub README for each RewardType)\r\n")]
         public RewardEnum Reward { get; set; } = RewardEnum.None;
 
+        [Description("The name of the objective (e.g, if your objective is on escaping, you can call it something like 'Escape The Facility')")]
         public string ObjectiveString { get; set; } = "Do Absolutely Nothing";
 
+        [Description("The amount that is needed to complete an objective.")]
         public int NeededToComplete { get; set; } = 0;
 
+        [Description("The ItemType you'll get if you complete an objective (if reward is set to Item)")]
         public ItemType RewardItem { get; set; } = ItemType.None;
 
+        [Description("The CustomItem (by ID) you'll get if you complete an objective (if reward is set to CustomItem)")]
         public uint RewardCustomItem { get; set; } = 0;
-        
+
+        [Description("The amount of XP you'll get if you complete an objective. (if reward is set to XP)")]
         public int RewardXP { get; set; } = 0;
     }
 }
